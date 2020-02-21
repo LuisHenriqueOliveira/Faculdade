adcfb1b108ff25de7d2be66c6e702cc651beb33d
arqdsis <arqdsis@MOOLI10130.animaedu.intranet>
2020-02-21 17:06:59 -0300

    Captura nome e exibe. Exercicio pronto.

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,8 @@
+import javax.swing*
 public class EntregaGit{
 	public static void main (String[] args){
 		System.out.println("Entregando tarefas com o Git na linha de comando...");
+		String nome = jOptionPane.showInputDialog("Qual seu nome?");
+		jOptionPane.showMessageDialog(null, "Oi, " + nome);
 	}
 }
